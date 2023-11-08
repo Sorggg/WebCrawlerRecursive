08a3d529073f62be1b8421af38094082093d7567
gaetanosorgente <8293354@itisrossi.vi.it>
2023-11-08 15:24:18 +0100

    fase embrionale

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        String seed = "";
+        crawl(seed, 2);
+    }
+
+    public static void crawl(String seed, int count) {
+        if (count != 0) {
+            //stampa a schermo tutti i siti della pagina
+            //applicare il metodo crawl a tutti i siti trovati
+        }
+    }
+}
