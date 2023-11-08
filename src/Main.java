cb622385579bc37c98fb952fda8773e7d3f7a9eb
gaetanosorgente <8293354@itisrossi.vi.it>
2023-11-08 15:51:17 +0100

    funziona ma ripete i link

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class Main {
 
     public static void main(String[] args) {
-        String seed = "";
-        crawl(seed, 2);
+        String seed = "https://yssupon.github.io/";
+        ArrayList<String> visited = new ArrayList<>();
+        crawl(seed, 2,visited);
     }
 
-    public static void crawl(String seed, int count) {
+    public static void crawl(String seed, int count,List<String> visited) {
         if (count != 0) {
-            //stampa a schermo tutti i siti della pagina
-            //applicare il metodo crawl a tutti i siti trovati
+            Document doc = request(seed, visited);
+            if (doc == null) {
+                return;
+            }
+            for (Element link : doc.select("a[href]")) {
+
+                String nextLink = link.absUrl("href");
+                crawl( nextLink, count-1,visited);
+            }
+        }
+    }
+    private static Document request(String url, List<String> visited) {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            System.out.println("Title: " + doc.title());
+            System.out.println("URL: " + url);
+            visited.add(url);
+            return doc;
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
         }
     }
 }
